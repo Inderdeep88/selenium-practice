43930493eff3f4ac91e83375cae2d9dfa4593ef2
Inderdeep Singh <inderdeep.singh@olx.com>
2023-03-28 01:00:44 +0530

    Added remotewebdriver functionality

--- src/main/java/com/newtours/WebDriverManager.java
+++ src/main/java/com/newtours/WebDriverManager.java
@@ -3,6 +3,11 @@ package com.newtours;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class WebDriverManager {
     private static WebDriver webDriver = null;
@@ -17,18 +22,51 @@ public class WebDriverManager {
         return webDriver;
     }
 
-    private static WebDriver createDriverInstance(){
-        if (System.getProperty("browser").equals("chrome")){
+    private static WebDriver createDriverInstance() {
+        if (System.getProperty("host") != null && System.getProperty("host").equals("grid")) {
+            return createRemoteWebDriver();
+        }
+        else {
+            return createLocalWebDriver();
+        }
+    }
+
+    private static WebDriver createLocalWebDriver(){
+        WebDriver localWebDriver;
+        if (System.getProperty("browser") != null && System.getProperty("browser").equals("chrome")){
             System.setProperty("webdriver.chrome.driver", webDriverBasePath + "chromedriver");
             System.setProperty("webdriver.chrome.logfile", webDriverBasePath + "chromedriver.log");
             System.setProperty("webdriver.chrome.verboseLogging", "true");
-            return new ChromeDriver();
-        } else if (System.getProperty("browser").equals("firefox")){
+            localWebDriver = new ChromeDriver();
+        }else if (System.getProperty("browser") != null && System.getProperty("browser").equals("firefox")){
             System.setProperty("webdriver.gecko.driver", webDriverBasePath + "geckodriver");
-            return new FirefoxDriver();
+            localWebDriver = new FirefoxDriver();
         } else {
             System.setProperty("webdriver.chrome.driver", webDriverBasePath + "chromedriver");
-            return new ChromeDriver();
+            localWebDriver = new ChromeDriver();
         }
+        return localWebDriver;
+    }
+    private static WebDriver createRemoteWebDriver() {
+        DesiredCapabilities dc ;
+        WebDriver remoteWebDriver = null;
+        String url = "http://"+System.getProperty("hubUrl")+":4444/wd/hub";
+
+        if (System.getProperty("browser") != null && System.getProperty("browser").equals("chrome")){
+            dc = DesiredCapabilities.chrome();
+            try {
+                remoteWebDriver = new RemoteWebDriver(new URL(url),dc);
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
+        } else if (System.getProperty("browser") != null && System.getProperty("browser").equals("firefox")){
+            dc = DesiredCapabilities.firefox();
+            try {
+                remoteWebDriver = new RemoteWebDriver(new URL(url), dc);
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return remoteWebDriver;
     }
 }
